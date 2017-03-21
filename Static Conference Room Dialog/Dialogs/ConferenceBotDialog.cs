140170cdcf4f19e00ae85375c135bbac18cc403c
MayurHC <mayur@orioninc.com>
2017-03-21 13:39:25 -0400

    Conference Room with Dialog
    Booking conference Room with Dialog

---
+++ Static Conference Room Dialog/Dialogs/ConferenceBotDialog.cs
@@ -0,0 +1,39 @@
+﻿using Bot_Application2.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot_Application2.Dialogs
+{
+    public class ConferenceBotDialog
+    {
+        public static readonly IDialog<string> dialog = Chain.PostToChain()
+                .Select(msg => msg.Text)
+                .Switch(
+                     new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase), (context, txt) =>
+                     {
+                         return Chain.ContinueWith(new GreetingDialog(), AfterGreetingContinuation);
+                     }),
+                     new DefaultCase<string, IDialog<string>>((context, txt) =>
+                     {
+                         return Chain.ContinueWith(FormDialog.FromForm(ConferenceReservation.BuildForm, FormOptions.PromptInStart), AfterGreetingContinuation);
+                     }))
+         .Unwrap()
+         .PostToUser();
+
+        private async static Task<IDialog<string>> AfterGreetingContinuation(IBotContext context, IAwaitable<object> res)
+        {
+            var token = await res;
+            var name = "User";
+            context.UserData.TryGetValue<string>("Name", out name);
+            return Chain.Return($"Thank you for using the Conference Room Reservation bot: {name}");
+        }
+    }
+
+
+}
---
+++ Static Conference Room Dialog/Models/ConferenceReservation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application2.Models
+{   
+    public enum ConferenceRoom
+	{
+		ConferenceA,
+		ConferenceB,
+		ConferenceC,
+		ConferenceD
+	}
+
+	public enum AmmenitiesOptions
+	{
+        Example,
+		Projector,
+		WhiteBoard,
+		ConferencePhone,
+		Wifi
+     
+	}
+    [Serializable]
+    public class ConferenceReservation
+    {
+        public ConferenceRoom? Conference;
+        public int? NumberOfEmployees;
+        public DateTime? CheckInDate;
+        public double? TotaltimeReq;
+        public List<AmmenitiesOptions> Ammenities;
+
+        public static IForm<ConferenceReservation> BuildForm()
+        {
+            return new FormBuilder<ConferenceReservation>()
+                .Message("Welcome to the Conference Room reservation bot!")
+                .Build();
+        }
+
+    }
+}
