9c3cd5ef149150dd1d757660949a7c29a9f9664a
MayurHC <mayur@orioninc.com>
2017-03-21 20:20:00 -0400

    Stock Project
    Stock Project without LUIS

---
+++ Stock/Controllers/MessagesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace Stock
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                string stockValue = await GetStock(activity.Text);
+
+                Activity reply = activity.CreateReply(stockValue);
+                await connector.Conversations.ReplyToActivityAsync(reply);   
+                
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Method to get the stock information for the provided symbol
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        private async Task<String> GetStock(string symbol)
+        {
+            return  await Stock.Modules.YahooStockServiceBot.GetStockRateAsync(symbol);     
+            
+        }
+    }
+}
