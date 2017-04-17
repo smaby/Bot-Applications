a7b102b9e088ce5b5bc0887df03093ff7c8a6ed3
MayurHC <mayur@orioninc.com>
2017-04-17 19:01:27 -0400

    Conference Room Booking
    Conference Room Booking

---
+++ EmojiBot/EmojiBot/CognitiveService/Utilities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.ProjectOxford.Emotion; 
+
+namespace EmojiBot.Utilities
+{
+    public class EmojiUtilities
+    {
+
+        private static readonly EmotionServiceClient emotionClientService = new EmotionServiceClient("f18b29b86cbe49e993bdf2db5a70d622");
+        /// <summary>
+        /// Method to get the emotions depending on the images shared
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static async Task<EmotionResult[]> CheckEmotion(string query)
+        {
+                     
+            HttpClient client = new HttpClient();
+            var  QueryString = HttpUtility.ParseQueryString(string.Empty);
+            string responsestring = string.Empty;
+
+            // Key from Microsoft cognitive serivice for emotion API. Key should be generated from the Azure portal. 
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{f18b29b86cbe49e993bdf2db5a70d622}");
+
+            //API for the Emoji provided by Congitive service of MS
+            string cognitiveEmotionAPI = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?" + QueryString;
+
+            //var uri = string.Format(cognitiveEmotionAPI, QueryString);
+
+            HttpResponseMessage response = null;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(query);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(cognitiveEmotionAPI, content).ConfigureAwait(false)  ;
+            }
+
+            responsestring = await response.Content.ReadAsStringAsync();
+            EmotionResult[] faces = JsonConvert.DeserializeObject<EmotionResult[]>(responsestring);
+
+            return faces;
+        }
+    }
+
+    public class scores
+    {
+        public double anger { get; set; }
+        public double contempt { get; set; }
+        public double disgust { get; set; }
+        public double fear { get; set; }
+        public double happiness { get; set; }
+        public double neutral { get; set; }
+        public double sadness { get; set; }
+        public double surprise { get; set; }
+    }
+
+    public class EmotionResult
+    {
+        public scores scores { get; set; }
+    }
+}
---
+++ EmojiBot/EmojiBot/Controllers/MessagesController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using EmojiBot.Utilities;
+
+namespace EmojiBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                Activity reply = null;
+                if (activity.Attachments.Count > 0)
+                {
+
+                    String emotionstext = "No emotions";
+                    double dblexpression = 0.0;
+                    System.Text.StringBuilder emotionsCollection = new System.Text.StringBuilder();
+
+                    EmotionResult[] emotionResult = await Utilities.EmojiUtilities.CheckEmotion(activity.Attachments[0].ContentUrl);
+
+                    if (emotionResult.Count() >0)
+                    {
+                        emotionsCollection.Append("Anger");
+                        emotionsCollection.Append(emotionResult[0].scores.anger);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("contempt");
+                        emotionsCollection.Append(emotionResult[0].scores.contempt);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("disgust");
+                        emotionsCollection.Append(emotionResult[0].scores.disgust);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("fear");
+                        emotionsCollection.Append(emotionResult[0].scores.fear);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("happiness");
+                        emotionsCollection.Append(emotionResult[0].scores.happiness);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("neutral");
+                        emotionsCollection.Append(emotionResult[0].scores.neutral);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("sadness");
+                        emotionsCollection.Append(emotionResult[0].scores.sadness);
+                        emotionsCollection.AppendLine();
+
+                        emotionsCollection.Append("surprise");
+                        emotionsCollection.Append(emotionResult[0].scores.surprise);
+                        emotionsCollection.AppendLine();
+
+
+                        if (emotionResult[0].scores.anger > 0)
+                        {
+                            dblexpression = emotionResult[0].scores.anger;
+                            emotionstext = "anger";
+                        }
+                        if (emotionResult[0].scores.contempt > dblexpression)
+                        {
+                            emotionstext = "contempt";
+                            dblexpression = emotionResult[0].scores.contempt;
+                        }
+                                              
+                        if (emotionResult[0].scores.disgust > dblexpression)
+                        {
+                            emotionstext = "disgust";
+                            dblexpression = emotionResult[0].scores.disgust;
+                        }
+                        if (emotionResult[0].scores.fear > dblexpression)
+                        {
+                            emotionstext = "fear";
+                            dblexpression = emotionResult[0].scores.fear;
+                        }
+
+                        if (emotionResult[0].scores.happiness > dblexpression)
+                        {
+                            emotionstext = "happiness";
+                            dblexpression = emotionResult[0].scores.happiness;
+                        }
+
+                        if (emotionResult[0].scores.neutral > dblexpression)
+                        {
+                            emotionstext = "neutral";
+                            dblexpression = emotionResult[0].scores.neutral;
+                        }
+
+                        if (emotionResult[0].scores.sadness > dblexpression)
+                        {
+                            emotionstext = "sadness";
+                            dblexpression = emotionResult[0].scores.sadness;
+                        }
+                        else if (emotionResult[0].scores.surprise > dblexpression)
+                        {
+                            emotionstext = "surprise";
+                            dblexpression = emotionResult[0].scores.surprise;
+                        }
+                                                                       
+                    }
+                    else
+                    {
+                        emotionstext= "No images found";
+                    }
+
+                    emotionsCollection.AppendLine();
+                    emotionsCollection.Append("------------------------");
+                    emotionsCollection.Append(emotionstext); 
+
+                    reply = activity.CreateReply(emotionsCollection.ToString());
+                  }
+                else
+                {
+                    reply = activity.CreateReply("Please add images");
+                }
+                
+                // return our reply to the user
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+
+ }
